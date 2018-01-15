bb3c275c0630191d4847e96adcc803c5a8d91b14
fedmask <33149307+fedmask@users.noreply.github.com>
2018-01-15 11:11:36 +0100

    Update input.js

--- WebRoot/WEB-INF/jsps/emp/input.js
+++ WebRoot/WEB-INF/jsps/emp/input.js
@@ -3,15 +3,17 @@
  * @returns
  */
 $(function() {
-		var checked1 = $("#all").attr("checked")=="checked";
-		var banana = $(this).attr("checked", !$(this).attr("checked"));
-		$("#all").click(function() {
-			$("[name=roles]:checkbox").attr("checked", checked1);
-		});
-		$("#reverse").click(function() {
-			$("[name=roles]:checkbox").each(function () {
-                banana;
-            });
+	var checked1 = $("#all").attr("checked")=="checked";
+	$("#all").click(function() {
+		$("[name=roles]:checkbox").attr("checked", checked1);
+	});
+});
 
-		});
+$(function despacito() {
+	var banana = $(this).attr("checked", !$(this).attr("checked"));
+	$("#reverse").click(function() {
+		$("[name=roles]:checkbox").each(function () {
+            banana;
+        });
+	});
 });
